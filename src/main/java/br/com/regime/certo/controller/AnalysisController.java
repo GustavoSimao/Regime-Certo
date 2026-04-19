80663846fa9cbca775507edbdf2768000e8acdaf
Gusta <gustavodea.simao@hotmail.com>
2026-04-19 11:46:47 -0300

    fix: corrige .gitignore e remove arquivos ignorados do rastreamento

--- src/main/java/br/com/regime/certo/controller/AnalysisController.java
+++ src/main/java/br/com/regime/certo/controller/AnalysisController.java
@@ -4,6 +4,7 @@ import br.com.regime.certo.dto.AnalysisResponse;
 import br.com.regime.certo.dto.RevenueRequest;
 import br.com.regime.certo.services.AnalysisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,5 +14,11 @@ public class AnalysisController {
 
     @Autowired
     AnalysisService analysisService;
+
+    @PostMapping
+    public ResponseEntity<AnalysisResponse> analyze(@RequestBody RevenueRequest request) {
+        AnalysisResponse response = analysisService.performAnalysis(request);
+        return ResponseEntity.ok(response);
+    }
 
 }
